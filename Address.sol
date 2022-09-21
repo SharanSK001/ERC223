f455fe58935cade962c3dff8273b20d56c0a4c2c
SharanSK001 <111493804+SharanSK001@users.noreply.github.com>
2022-09-21 14:59:45 +0530

    Add files via upload

---
+++ Address.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.8.0;
+
+library Address {
+    
+    function isContract(address account) internal view returns (bool) {
+        
+
+        uint256 size;
+       
+        assembly { size := extcodesize(account) }
+        return size > 0;
+    }
+
+
+    function toPayable(address account) internal pure returns (address payable) {
+        return payable(account);
+    }
+}
